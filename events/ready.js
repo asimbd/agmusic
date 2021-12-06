a79beb82c27f200a7540ef68f8440a7649e6b2f4
Abdur Rahaman Asim <72205854+asimbd@users.noreply.github.com>
2021-12-06 20:38:26 +0600

    Update

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,4 @@
 module.exports = (client) => {
   console.log("[Discord API]: Logged In As " + client.user.tag);
-  client.user.setActivity("Created By Asim | ag/help");
+  client.user.setActivity(`Currently in ${client.guilds.cache.size} servers`);
 };
